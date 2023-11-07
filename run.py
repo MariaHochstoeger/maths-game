d658094ede6187dec4183f8e715f53ca6adeca69
Maria Hochstoeger <m_hochstoeger@hotmail.com>
2023-11-07 14:47:17 +0000

    Write welcome message and rules. Ask for username and validate input to not be empty

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+print("Welcome to the Maths Game!")
+print("Here you can test your knowledge in maths.")
+print("You can choose which mathematical operation you would like to practice.")
+print("You will play 5 rounds. Then you get your score.")
+print("Let's start!\n")
+
+def get_username():
+    username = ""
+    username = input("Please enter your name: ").strip()
+
+    while username == "":
+        username = input("You did not enter anything. Please enter a valid name: ")
+
+    print(f"\nHello {username}!\n")
+    return username
+
+get_username()
